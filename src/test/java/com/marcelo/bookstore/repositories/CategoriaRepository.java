f98d84964114656ff9593f98d6158baeb5e08df0
= <=>
2022-07-19 14:12:35 -0300

    Criação de repositories

--- src/test/java/com/marcelo/bookstore/repositories/CategoriaRepository.java
+++ src/test/java/com/marcelo/bookstore/repositories/CategoriaRepository.java
@@ -1,5 +1,11 @@
 package com.marcelo.bookstore.repositories;
 
-public class CategoriaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcelo.bookstore.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
 
 }
