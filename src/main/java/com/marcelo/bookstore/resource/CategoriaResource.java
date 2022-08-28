647295f2d3422e66911c09dd15faeacc7074374f
= <=>
2022-08-28 18:24:42 -0300

    Post para categoria

--- src/main/java/com/marcelo/bookstore/resource/CategoriaResource.java
+++ src/main/java/com/marcelo/bookstore/resource/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.marcelo.bookstore.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcelo.bookstore.domain.Categoria;
 import com.marcelo.bookstore.dtos.CategoriaDTO;
@@ -37,4 +41,10 @@ public class CategoriaResource {
 		
 	}
 	
+	@PostMapping()
+	public ResponseEntity<Categoria> save(@RequestBody Categoria categoria){
+		 categoria = categoriaService.save(categoria);
+		 URI  uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).body(categoria);
+	}
 }
--- src/main/java/com/marcelo/bookstore/service/CategoriaService.java
+++ src/main/java/com/marcelo/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.marcelo.bookstore.domain.Categoria;
@@ -24,6 +25,12 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
+	}
+
+
+	public Categoria save(Categoria categoria) {
+		categoria.setId(null);
+		return categoriaRepository.save(categoria);
 	}
 	
 	
