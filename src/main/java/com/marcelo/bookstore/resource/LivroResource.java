93c64fb3df618fe842faa74a1a018e71873d56f2
= <=>
2022-09-14 16:26:03 -0300

    Criação de recurso para atualização de obj atravres via verbo PUT e PATCH

--- src/main/java/com/marcelo/bookstore/resource/LivroResource.java
+++ src/main/java/com/marcelo/bookstore/resource/LivroResource.java
@@ -6,8 +6,12 @@ import java.util.stream.Collectors;
 import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +40,19 @@ public class LivroResource{
 		List<Livro> lista = service.findAllLivroByCategoria(id_categoria);
 		List<LivroDto> livroDto = lista.stream().map(l-> new LivroDto(l)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(livroDto);
+	}
+	
+	
+	@PutMapping(value = "/{id}")
+	ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
+		Livro newLivro = service.update(id, obj);
+		return ResponseEntity.ok().body(newLivro);
+	}
+	
+	
+	@PatchMapping(value = "/{id}")
+	ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj){
+		Livro newLivro = service.update(id, obj);
+		return ResponseEntity.ok().body(newLivro);
 	}
 }
--- src/main/java/com/marcelo/bookstore/service/LivroService.java
+++ src/main/java/com/marcelo/bookstore/service/LivroService.java
@@ -2,6 +2,7 @@ package com.marcelo.bookstore.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,15 +26,28 @@ public class LivroService {
 	
 	public Livro findById (Integer id){
 		  Optional<Livro> livro = repository.findById(id);
-		  return livro.orElseThrow(()-> new ObjectNotFoundException("Não foi encontrado nenhum livro para o id : " + id + 
-		  		 " e tipo " + Livro.class.getName()));
+		  return livro.orElseThrow(()-> new ObjectNotFoundException("Não foi encontrado nenhum livro para o id : " + id + " e tipo " + Livro.class.getName()));
 	}
 
 
 	public List<Livro> findAllLivroByCategoria(Integer id_categoria) {
 		categoriaRepository.findById(id_categoria);
 		return repository.findAllLivroByCategoria(id_categoria);
-		
+	}
+
+
+	public Livro update(Integer id, Livro obj) {
+		 Livro livro =  findById(id);
+		 livro = setPropriedadesLivro(livro, obj);
+		 return livro;
+	}
+
+
+	private Livro setPropriedadesLivro(Livro livro, Livro obj) {
+		 livro.setTitulo(obj.getTitulo());
+		 livro.setNome_autor(obj.getNome_autor());
+		 livro.setTexto(obj.getTexto());
+		 return repository.save(livro);
 	}
 	
 	
